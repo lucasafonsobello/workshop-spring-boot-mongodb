74852aefd488fdf93ba3fd84e24f8fb788652dbc
Lucas Afonso Bello <lucasafonsobello@gmail.com>
2018-12-20 21:47:11 -0200

    Consulta simples com query methods

--- src/main/java/com/lucasafonsobello/workshopmongo/services/PostService.java
+++ src/main/java/com/lucasafonsobello/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.lucasafonsobello.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,10 @@ public class PostService {
 	}
 	
 
-	
+	public List<Post> findByTitle(String text){
+		
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 	
 	
 }
